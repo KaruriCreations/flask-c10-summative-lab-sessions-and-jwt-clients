3eac234e6510dbbca48d81b8e3fc83d4d162550b
KaruriCreations <266662126+KaruriCreations@users.noreply.github.com>
2026-08-01 12:52:45 +0300

    added my notes schema

--- server/schemas.py
+++ server/schemas.py
@@ -5,3 +5,8 @@ class UserSchema(Schema):
     username = fields.Str(required=True, validate=validate.Length(min=3))
     password = fields.Str(load_only=True, required=True, validate=validate.Length(min=4))
 
+class NoteSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.Str(required=True, validate=validate.Length(min=1))
+    content = fields.Str(required=True)
+    user_id = fields.Int(dump_only=True)
