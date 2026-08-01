7a9359efc78b1998b565d7330f825a3c169b1709
KaruriCreations <266662126+KaruriCreations@users.noreply.github.com>
2026-08-01 14:21:27 +0300

    imported my models and sche,as

--- server/app.py
+++ server/app.py
@@ -2,3 +2,6 @@ import os
 from flask import Flask, request, session
 from flask_restful import Api, Resource
 from flask_migrate import Migrate
+
+from models import db, bcrypt, User, Note
+from schemas import user_schema, note_schema, notes_schema
