d621d9d173abb14b4c5ef18dd118570307b0faaf
KaruriCreations <266662126+KaruriCreations@users.noreply.github.com>
2026-08-01 14:22:59 +0300

    instatiated my my db,migrate and bcrypt

--- server/app.py
+++ server/app.py
@@ -5,3 +5,13 @@ from flask_migrate import Migrate
 
 from models import db, bcrypt, User, Note
 from schemas import user_schema, note_schema, notes_schema
+
+app =Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = os.environ.get('SECRET_KEY', 'vincent-maina-final-project5')
+
+db.init_app(app)
+bcrypt.init_app(app)
+migrate = Migrate(app, db)
+api = Api(app)
