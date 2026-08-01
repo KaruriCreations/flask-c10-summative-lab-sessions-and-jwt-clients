4da1ab60a346b19832b11472f212c255b1f9535a
KaruriCreations <266662126+KaruriCreations@users.noreply.github.com>
2026-08-01 14:27:24 +0300

    created my signup endpoint

--- server/app.py
+++ server/app.py
@@ -23,3 +23,17 @@ def get_current_user():
         return User.query.get(user_id)
     return None
 
+#auth endpoints
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+        errors = user_schema.validate(data)
+        if errors:
+            return errors, 400
+        if User.query.filter_by(username=data['username']).first():
+            return {'error': 'Username already exists'}, 400
+        user = User(username=data['username'], password=data['password'])
+        db.session.add(user)
+        db.session.commit()
+        session['user_id'] = user.id
+        return user_schema.dump(user), 201
