da969690586b46435f37eb7788627540ddb0381d
KaruriCreations <266662126+KaruriCreations@users.noreply.github.com>
2026-08-02 14:18:40 +0300

    add and commit users to the db

--- server/seed.py
+++ server/seed.py
@@ -9,3 +9,6 @@ with app.app_context():
     print("Creating test users...")
     user1 = User(username="alice", password_hash="password123")
     user2 = User(username="bob", password_hash="password123")
+
+    db.session.add_all([user1, user2])
+    db.session.commit()
