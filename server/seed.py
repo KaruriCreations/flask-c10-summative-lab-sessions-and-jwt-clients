91906a64a39f506a400b8cd41541cde168401c3b
KaruriCreations <266662126+KaruriCreations@users.noreply.github.com>
2026-08-02 14:22:08 +0300

    i needed to hash the passwords first

--- server/seed.py
+++ server/seed.py
@@ -7,8 +7,11 @@ with app.app_context():
     User.query.delete()
 
     print("Creating test users...")
-    user1 = User(username="alice", password_hash="password123")
-    user2 = User(username="bob", password_hash="password123")
+    user1 = User(username="alice")
+    user1.password_hash = "password123"
+
+    user2 = User(username="bob")
+    user2.password_hash = "password123"
 
     db.session.add_all([user1, user2])
     db.session.commit()
